62cdb2ec0273ae6f41b4c20469a643bacbcda42b
Josh Black <joshblack@us.ibm.com>
2016-05-03 23:13:35 -0500

    Add all instructor base information

--- src/data/instructors.js
+++ src/data/instructors.js
@@ -93,7 +93,349 @@ const instructors = [
     "videos": [
       "d852c265"
     ]
-  }
+  },
+  {
+    "id": "cb9d8bd1",
+    "name": "Craig McKeachie",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/036/bio/craig-mckeachie-headshot.png?1447939149",
+    "videos": []
+  },
+  {
+    "id": "fb3a9a9e",
+    "name": "Thomas Burleson",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/006/bio/ThomasB.jpg?1387407307",
+    "videos": []
+  },
+  {
+    "id": "3b1bc9a7",
+    "name": "Merrick Christensen",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/038/bio/aChLd0Cx.jpeg?1448058267",
+    "videos": []
+  },
+  {
+    "id": "6de62241",
+    "name": "Jacob Carter",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/028/bio/profile-pic.jpg?1455693549",
+    "videos": []
+  },
+  {
+    "id": "890b832c",
+    "name": "Will Button",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/014/bio/Will_headshot.png?1426020297",
+    "videos": []
+  },
+  {
+    "id": "feef4c25",
+    "name": "Ben Clinkinbeard",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/001/bio/headShot_large.jpg?1387142514",
+    "videos": []
+  },
+  {
+    "id": "01772fca",
+    "name": "David Tucker",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/061/bio/david-tucker-headshot-500.jpeg?1457718712",
+    "videos": []
+  },
+  {
+    "id": "129ecf89",
+    "name": "Preethi Kasireddy",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/042/bio/11895272_10154078186552506_3411075844880369856_o.jpg?1451517982",
+    "videos": []
+  },
+  {
+    "id": "21684e50",
+    "name": "Trevor Miller",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/027/bio/FullSizeRender.jpg?1458427425",
+    "videos": []
+  },
+  {
+    "id": "2e2d5197",
+    "name": "Brett Shollenberger",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/002/bio/brett.jpg?1387142580",
+    "videos": []
+  },
+  {
+    "id": "c5dfb210",
+    "name": "Ronn Ross",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/049/bio/Ronn_Ross_500x500.jpg?1452481580",
+    "videos": []
+  },
+  {
+    "id": "c32a9553",
+    "name": "Murphy Randle",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/051/bio/Avatar2.jpg?1453245799",
+    "videos": []
+  },
+  {
+    "id": "5ea09859",
+    "name": "Mykola Bilokonsky",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/031/bio/IMG_0057.jpg?1444527536",
+    "videos": []
+  },
+  {
+    "id": "4537c7c2",
+    "name": "Joe Maddalone",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/007/bio/Cropped-Portrait-Attempt.jpg?1391746899",
+    "videos": []
+  },
+  {
+    "id": "14e4a575",
+    "name": "Tim Kindberg",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/010/bio/IMG_4220.JPG?1404257177",
+    "videos": []
+  },
+  {
+    "id": "2f3e0c15",
+    "name": "Brian Troncone",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/064/bio/brian_troncone.png?1458090709",
+    "videos": []
+  },
+  {
+    "id": "44912e94",
+    "name": "Shane Osbourne",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/030/bio/Untitled-1.jpg?1441957411",
+    "videos": []
+  },
+  {
+    "id": "891569ff",
+    "name": "Ari Picker",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/056/bio/ari-avatar.jpg?1455483682",
+    "videos": []
+  },
+  {
+    "id": "333887a3",
+    "name": "Erik Aybar",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/025/bio/ErikAybar_500x500.jpeg?1435600125",
+    "videos": []
+  },
+  {
+    "id": "aa46731d",
+    "name": "Robert Penner",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/016/bio/Robert-Penner-headshot-500x500.jpg?1426554478",
+    "videos": []
+  },
+  {
+    "id": "d99a1276",
+    "name": "Scott Moss",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/015/bio/scott_headshot.jpg?1426531840",
+    "videos": []
+  },
+  {
+    "id": "dc12eaff",
+    "name": "Simon Bailey",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/018/bio/simon-headshot.jpg?1430674598",
+    "videos": []
+  },
+  {
+    "id": "48132e7e",
+    "name": "Jeremy Robertson",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/023/bio/HeadshotCrop3.png?1434059159",
+    "videos": []
+  },
+  {
+    "id": "e7e03ee9",
+    "name": "Benjamin Roth",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/021/bio/tete_bis_mod.jpg?1433374782",
+    "videos": []
+  },
+  {
+    "id": "607c0e68",
+    "name": "Ben Lesh",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/019/bio/11402677_10153292063120659_5138577124041649571_o_%281%29.jpg?1459217701",
+    "videos": []
+  },
+  {
+    "id": "0c6e55f2",
+    "name": "Kent C. Dodds",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/011/bio/photo.png?1425176578",
+    "videos": []
+  },
+  {
+    "id": "7e26d704",
+    "name": "Mike Hartington",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/062/bio/mike-headshot.png?1457958228",
+    "videos": []
+  },
+  {
+    "id": "6e4b9bd1",
+    "name": "Keith Peters",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/013/bio/eggheadphoto.png?1421766974",
+    "videos": []
+  },
+  {
+    "id": "70495e20",
+    "name": "Tyler McGinnis",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/017/bio/tylermcginnis.png?1427213937",
+    "videos": []
+  },
+  {
+    "id": "3c0a7949",
+    "name": "Phil Holden",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/033/bio/IMG_3123-small.JPG?1445691628",
+    "videos": []
+  },
+  {
+    "id": "c0b55693",
+    "name": "Aaron Frost",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/009/bio/UCvQytmY.jpeg?1401981826",
+    "videos": []
+  },
+  {
+    "id": "e2f9f5c5",
+    "name": "Jafar Husain",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/012/bio/image1.jpeg?1420665163",
+    "videos": []
+  },
+  {
+    "id": "18241123",
+    "name": "Josh Black",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/058/bio/XJGtEzxg.jpeg?1457387913",
+    "videos": []
+  },
+  {
+    "id": "223c0911",
+    "name": "Lukas Ruebbelke",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/008/bio/lukas-warm.jpg?1395280602",
+    "videos": []
+  },
+  {
+    "id": "32a5c470",
+    "name": "Garth Braithwaite",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/026/bio/park_avatar.jpg?1437494877",
+    "videos": []
+  },
+  {
+    "id": "a68e2bff",
+    "name": "Mike Frey",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/020/bio/headshot.png?1432933593",
+    "videos": []
+  },
+  {
+    "id": "0e3df055",
+    "name": "André Staltz",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/024/bio/andre_500.jpg?1435599944",
+    "videos": []
+  },
+  {
+    "id": "278e6d1e",
+    "name": "Trevor Ewen",
+    "handle": "",
+    "bio": "",
+    "site": "",
+    "avatar": "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/004/bio/big_profile_image.jpg?1387142668",
+    "videos": []
+  },
 ];
 
 const { getAllOfInstructor, getInstructor } = resource('Instructor', instructors);
